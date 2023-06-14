d3acb115c774e63666d488059bdaf9534a438af1
SatrantBains <123198090+SatrantBains@users.noreply.github.com>
2023-06-14 14:36:17 -0400

    Add files via upload

---
+++ Resformer/glue_evaluation_cuda_encoder.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+# from google.colab import drive
+# drive.mount("/content/gdrive/")
+# root_path = "/content/gdrive/MyDrive/"
+# %cd "/content/gdrive/MyDrive/resformer"
+import copy
+import argparse
+import torch
+import torch.nn as nn
+import random
+import requests
+import datasets
+import transformers
+import pandas as pd
+import terrorch
+from terrorch import Injector
+import time
+from datasets import load_dataset, load_metric
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+print(transformers.__version__)
+
+def preprocess_function(examples):
+    if sentence2_key is None:
+        return tokenizer(examples[sentence1_key], truncation=True)
+    return tokenizer(examples[sentence1_key], examples[sentence2_key], truncation=True)
+
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = torch.Tensor(predictions)
+    if task != "stsb":
+        predictions = torch.argmax(predictions, axis=1)
+    else:
+        predictions = predictions[:, 0]
+    return metric.compute(predictions=predictions, references=labels)
+
+def evaluation(input_model):
+    evaluator = Trainer(input_model, eval_dataset=encoded_dataset[validation_key], tokenizer=tokenizer, compute_metrics=compute_metrics)
+    return evaluator.evaluate()
+    
+
+
+GLUE_TASKS = ["cola", "mnli", "mnli-mm", "mrpc", "qnli", "qqp", "rte", "sst2", "stsb", "wnli"]
+
+task_to_keys = {
+    "cola": ("sentence", None),
+    "mnli": ("premise", "hypothesis"),
+    "mnli-mm": ("premise", "hypothesis"),
+    "mrpc": ("sentence1", "sentence2"),
+    "qnli": ("question", "sentence"),
+    "qqp": ("question1", "question2"),
+    "rte": ("sentence1", "sentence2"),
+    "sst2": ("sentence", None),
+    "stsb": ("sentence1", "sentence2"),
+    "wnli": ("sentence1", "sentence2"),
+}
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--task', metavar='N', type=str)
+
+parser.add_argument('--model', metavar='N', type=str)
+
+parser.add_argument('--ber', metavar='N', type=str)
+
+parser.add_argument('--seed', metavar='N', type=int)
+
+args = parser.parse_args()
+
+task, model_and_task, bit_error_rate, seed = args.task, args.model, args.ber, args.seed
+
+device = torch.device('cpu')
+
+actual_task = "mnli" if task == "mnli-mm" else task
+
+validation_key = "validation_mismatched" if task == "mnli-mm" else "validation_matched" if task == "mnli" else "validation"
+
+metric_name = "pearson" if task == "stsb" else "matthews_correlation" if task == "cola" else "accuracy"
+
+num_labels = 3 if task.startswith("mnli") else 1 if task=="stsb" else 2
+
+url_test = 'https://huggingface.co/' + model_and_task + '/resolve/main/config.json'
+
+if requests.head(url_test).status_code != 404:
+
+    model_inj = AutoModelForSequenceClassification.from_pretrained(model_and_task, num_labels=num_labels)
+
+else:
+    
+    print("ERROR: selected model don't exist")
+    
+    exit()
+
+with torch.no_grad():
+
+    injector = Injector(param_names = ['encoder', 'transformer'], p = float(bit_error_rate), device = device, seed = seed, verbose = True)
+
+    injector.profiling(model_inj)
+
+    model_test0 = copy.deepcopy(model_inj)
+
+    injector.inject(model_inj)
+
+    model_test1 = copy.deepcopy(model_inj)
+
+    injector.correct(model_inj)
+
+    model_test2 = copy.deepcopy(model_inj)
+
+device = torch.device('cuda')
+
+model_test0.to(device)
+
+model_test1.to(device)
+
+model_test2.to(device)
+
+dataset = load_dataset("glue", actual_task)
+
+metric = load_metric('glue', actual_task)
+
+tokenizer = AutoTokenizer.from_pretrained(model_and_task)
+
+sentence1_key, sentence2_key = task_to_keys[task]
+
+encoded_dataset = dataset.map(preprocess_function, batched=True)
+
+# model_inj.to(device)
+
+with torch.no_grad():
+    # eval_result = evaluation(model_test0)
+    # print('................................................')
+    # print(bit_error_rate, eval_result)
+    # print('................................................')
+
+    eval_result = evaluation(model_test1)
+    print('................................................')
+    print(bit_error_rate, eval_result)
+    print('................................................')
+
+    eval_result = evaluation(model_test2)
+    print('................................................')
+    print(bit_error_rate, eval_result)
+    print('................................................')
---
+++ Resformer/sum_evaluation_cuda_embedding.py
@@ -0,0 +1,145 @@
+import copy
+import nltk
+import torch
+import torch.nn as nn
+import random
+import argparse
+import datasets
+import transformers
+import numpy as np
+import pandas as pd
+import terrorch
+from terrorch import Injector
+import time
+from datasets import load_dataset, load_metric
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+print(transformers.__version__)
+
+nltk.download('punkt')
+
+def preprocess_function(examples):
+    inputs = [prefix + doc for doc in examples["document"]]
+    model_inputs = tokenizer(inputs, max_length=max_input_length, truncation=True)
+
+    # Setup the tokenizer for targets
+    with tokenizer.as_target_tokenizer():
+        labels = tokenizer(examples["summary"], max_length=max_target_length, truncation=True)
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    # Replace -100 in the labels as we can't decode them.
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    
+    # Rouge expects a newline after each sentence
+    decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds]
+    decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
+    
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+    # Extract a few results
+    result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+    
+    # Add mean generated length
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
+    result["gen_len"] = np.mean(prediction_lens)
+    
+    return {k: round(v, 4) for k, v in result.items()}
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--task', metavar='N', type=str)
+
+parser.add_argument('--model', metavar='N', type=str)
+
+parser.add_argument('--ber', metavar='N', type=float)
+
+parser.add_argument('--seed', metavar='N', type=str)
+
+args = parser.parse_args()
+
+task, model_and_task, bit_error_rate, seed = args.task, args.model, args.ber, args.seed
+
+task, model_and_task, bit_error_rate, seed = 'xsum', 'brutusxu/t5-base-finetuned-xsum', 1e-7, 0
+
+device = torch.device('cpu')
+
+raw_datasets = load_dataset(task)
+
+metric = load_metric("rouge")
+
+prefix = "summarize: "
+
+max_input_length = 1024
+
+max_target_length = 128
+    
+tokenizer = AutoTokenizer.from_pretrained(model_and_task)
+
+"""If you are using one of the five T5 checkpoints we have to prefix the inputs with "summarize:" (the model can also translate and it needs the prefix to know which task it has to perform)."""
+
+tokenized_datasets = raw_datasets.map(preprocess_function, batched = True)
+
+model_inj = AutoModelForSeq2SeqLM.from_pretrained(model_and_task)
+
+device = torch.device('cpu')
+
+with torch.no_grad():
+
+    injector = Injector(param_names = ['shared'], p = float(bit_error_rate), device = device, seed = seed, verbose = True)
+
+    injector.profiling(model_inj)
+
+    model_test0 = copy.deepcopy(model_inj)
+    
+    injector.inject(model_inj)
+
+    model_test1 = copy.deepcopy(model_inj)
+
+    injector.correct(model_inj)
+
+    model_test2 = copy.deepcopy(model_inj)
+
+device = torch.device('cuda')
+
+model_test0.to(device)
+
+model_test1.to(device)
+
+model_test2.to(device)
+
+batch_size = 32
+
+def eval(input_model):
+    args = Seq2SeqTrainingArguments(f"output", predict_with_generate = True,)
+    evaluator = Seq2SeqTrainer(input_model, args, eval_dataset=tokenized_datasets["validation"], data_collator=data_collator, tokenizer=tokenizer, compute_metrics=compute_metrics)
+    return evaluator.evaluate()
+
+data_collator = DataCollatorForSeq2Seq(tokenizer, model = model_test0)
+
+with torch.no_grad():
+    eval_result = eval(model_test0)
+    print('................................................')
+    print('Error free model', bit_error_rate, eval_result)
+    print('................................................')
+
+data_collator = DataCollatorForSeq2Seq(tokenizer, model = model_test1)
+
+with torch.no_grad():
+    eval_result = eval(model_test1)
+    print('................................................')
+    print('Error injected model', bit_error_rate, eval_result)
+    print('................................................')
+
+data_collator = DataCollatorForSeq2Seq(tokenizer, model = model_test2)
+
+with torch.no_grad():
+    eval_result = eval(model_test2)
+    print('................................................')
+    print('Error corrected model', bit_error_rate, eval_result)
+    print('................................................')
