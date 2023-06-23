1be3ab80df78ef62daeb6c8f77b7353ab038f654
liambaker10 <lbaker10@villanova.edu>
2023-06-23 16:08:27 -0400

    updated website 6/23 end of day

--- TestWebsite/website/tryGPT2.py
+++ TestWebsite/website/tryGPT2.py
@@ -13,13 +13,15 @@ def tryModel():
 @tryGPT2.route("/tryGPT2/get", methods=["GET", "POST"])  # type: ignore
 def chat():
     msg = request.form["msg"]
+    new_length = request.form["len"]
+    max_len = int(new_length)
     input = msg
-    return get_Chat_response(input)
+    return get_Chat_response(input, max_len)
 
 
-def get_Chat_response(text):
+def get_Chat_response(text, max_length=50):
     generator = pipeline("text-generation", model="gpt2")
-    output = generator(text, max_length=30, num_return_sequences=1)
+    output = generator(text, max_length=max_length, num_return_sequences=1)
     # formatted = output[21 : len(output) - 3].replace("/", "")
     formatted = output[0]["generated_text"]
     return formatted
