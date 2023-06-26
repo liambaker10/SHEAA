463d7671922f5d1280afc32c51772aec02edfb8d
afink213 <135249362+afink213@users.noreply.github.com>
2023-06-26 12:18:51 -0400

    Update injector.py

--- important functions/injector.py
+++ important functions/injector.py
@@ -1,59 +1,58 @@
-import transformers
 import torch
 import torch.nn as nn
-import sys
-import pprint
-import json
-import random
+import weightdistribution as wd  # Assuming weightdistribution is a custom module you have defined
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-def count_parameters(model: nn.Module) -> int:
-    total_params = sum(p.numel() for p in model.parameters())
-    return total_params
+def get_parameter_importance(model: nn.Module) -> dict:
+    parameter_importance = {}
+    for name, parameter in model.named_parameters():
+        parameter_importance[name] = torch.std(parameter).item()  # Calculate importance based on standard deviation
+    return parameter_importance
 
-def get_parameter_value_by_index(model, parameter_index):
-    parameter_value = None
-    model_parameters = list(model.parameters())
-    if parameter_index < len(model_parameters):
-        parameter_value = model_parameters[parameter_index].data
-    return parameter_value
+def modify_parameters(model: nn.Module, num_params: int, modification_func: callable):
+    parameter_importance = get_parameter_importance(model)
+    sorted_params = sorted(parameter_importance.items(), key=lambda x: x[1], reverse=True)
+    total_params = len(sorted_params)
 
-gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
-num_params = int(input("Enter the number of parameters to change: "))
-new_value = int(input("Enter a new value: "))
-x = random.randint(0, count_parameters(gpt2_model) - num_params)
+    if num_params > total_params:
+        num_params = total_params
 
-state_dict = gpt2_model.state_dict()  # Get the model's state_dict
+    selected_params = [param[0] for param in sorted_params]
+    modified_params = set()
 
-for i in range(x, x + num_params):
-    parameter_index_gpt2 = i
-    parameter_value = get_parameter_value_by_index(gpt2_model, parameter_index_gpt2)
-    if parameter_value is not None:
-        parameter_value.fill_(new_value)  # Modify the parameter value
+    for name, parameter in model.named_parameters():
+        if len(modified_params) < num_params and name in selected_params:
+            modified_params.add(name)
+            modified_parameter = modification_func(parameter)
+            parameter.data.copy_(modified_parameter)
+        else:
+            parameter.requires_grad_(False)
 
-# Create an instance of the modified model
-modified_model = GPT2LMHeadModel.from_pretrained('gpt2')
-modified_model.load_state_dict(state_dict, strict=False)  # Load the modified state_dict
-
-# Create a tokenizer for the model
+# Create the GPT-2 model and tokenizer
+model = GPT2LMHeadModel.from_pretrained('gpt2')
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
-# Set the device to run the model on (e.g., 'cuda' if you have a GPU)
+# Specify the number of distinct parameters to modify and the modification function
+num_params = int(input("How many parameters would you like to modify?"))  # Update the value here
+new_val = int(input("New value:"))
+modification_func = lambda parameter: parameter.clone().fill_(new_val)  # Example modification: set the parameter values to 0.0
+
+# Modify a specific number of distinct parameters of highest importance
+modify_parameters(model, num_params, modification_func)
+
+# Set the device to run the model on
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# Move the model to the specified device
-modified_model = modified_model.to(device)
-
-# Set the modified model to evaluation mode
-modified_model.eval()
+model = model.to(device)
 
 # Generate text using the modified model
 input_text = "Once upon a time"
 input_ids = tokenizer.encode(input_text, return_tensors='pt').to(device)
 with torch.no_grad():
-    output = modified_model.generate(input_ids, max_length=50, num_return_sequences=1)
+    output = model.generate(input_ids, max_length=50, num_return_sequences=1)
 
 # Decode and print the generated text
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 print("Generated text:")
 print(generated_text)
+
+wd.plot_weight_distribution("gpt2", model)
