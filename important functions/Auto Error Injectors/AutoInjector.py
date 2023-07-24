35f20672986b6f65124a6ea5b9fb4b6f1a15e759
afink213 <135249362+afink213@users.noreply.github.com>
2023-07-24 12:58:29 -0400

    Update AutoInjector.py

--- important functions/Auto Error Injectors/AutoInjector.py
+++ important functions/Auto Error Injectors/AutoInjector.py
@@ -7,7 +7,7 @@ def get_parameter_importance(model: nn.Module) -> dict:
             parameter_importance[name] = torch.std(parameter).item()  # Calculate importance based on standard deviation
         return parameter_importance
 
-    def modify_parameters(model: nn.Module, num_params: int, modification_func: callable):
+def modify_parameters(model: nn.Module, num_params: int, modification_func: callable):
         parameter_importance = get_parameter_importance(model)
         sorted_params = sorted(parameter_importance.items(), key=lambda x: x[1], reverse=True)
         total_params = len(sorted_params)
@@ -25,3 +25,5 @@ def get_parameter_importance(model: nn.Module) -> dict:
                 parameter.data.copy_(modified_parameter)
             else:
                 parameter.requires_grad_(False)
+
+modification_func = lambda parameter: parameter.clone().fill_(new_val)
