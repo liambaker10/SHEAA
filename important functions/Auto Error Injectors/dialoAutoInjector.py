0cbc71c780bc92fd6bf8db8a3edd7aec29fb50e1
afink213 <135249362+afink213@users.noreply.github.com>
2023-07-25 12:33:31 -0400

    Update dialoAutoInjector.py

--- important functions/Auto Error Injectors/dialoAutoInjector.py
+++ important functions/Auto Error Injectors/dialoAutoInjector.py
@@ -6,7 +6,7 @@ def DialoErrorInjector(num_params, new_val, user_input):
     def get_parameter_importance(model: nn.Module) -> dict:
         parameter_importance = {}
         for name, parameter in model.named_parameters():
-            parameter_importance[name] = torch.std(parameter).item()  # Calculate importance based on standard deviation
+            parameter_importance[name] = torch.std(parameter).item()  
         return parameter_importance
 
     def modify_parameters(model: nn.Module, num_params: int, modification_func: callable):
@@ -32,24 +32,19 @@ def DialoErrorInjector(num_params, new_val, user_input):
     tokenizer.padding_side = "left"
     model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
-    modification_func = lambda parameter: parameter.clone().fill_(new_val)  # Example modification: set the parameter values to 0.0
+    modification_func = lambda parameter: parameter.clone().fill_(new_val)
 
-    # Modify a specific number of distinct parameters of highest importance
     modify_parameters(model, num_params, modification_func)
 
-    # Set the device to run the model on
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
-# Let's chat for 5 lines
-    # encode the new user input, add the eos_token and return a tensor in Pytorch
+
     input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors="pt")
 
-# Generate a response using the DialoGPT model
     response_ids = model.generate(
         input_ids,
         max_length=1000,
         pad_token_id=tokenizer.eos_token_id)
-    # pretty print last ouput tokens from bot
     response = tokenizer.decode(response_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
     print("DialoGPT: ", response)
 
