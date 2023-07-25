2577de2a5af3724d9f160a064db6492eea4dc739
afink213 <135249362+afink213@users.noreply.github.com>
2023-07-25 12:36:22 -0400

    Update BitInjector.py

--- important functions/BitInjectors/BitInjector.py
+++ important functions/BitInjectors/BitInjector.py
@@ -3,22 +3,16 @@ import torch.nn as nn
 import random
 
 def flip_bits(tensor, num_bits):
-        # Convert the tensor to binary
         binary_tensor = tensor.byte()
-        
-        # Get the number of bits in each element
         num_bits_per_element = binary_tensor.numel() * 8
         
-        # Generate random bit indices to flip
         bit_indices = random.sample(range(num_bits_per_element), num_bits)
         
-        # Create a copy of the tensor to modify
         modified_tensor = tensor.clone()
         
-        # Flip the selected bits in the modified tensor
         for index in bit_indices:
-            element_index = index // 8  # Index of the element in the tensor
-            bit_offset = index % 8      # Offset of the bit within the element
+            element_index = index // 8 
+            bit_offset = index % 8      
             modified_tensor.view(-1)[element_index] = modified_tensor.view(-1)[element_index].to(torch.long) ^ (1 << bit_offset)
         
         return modified_tensor
